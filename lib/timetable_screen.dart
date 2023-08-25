ad6331f05e02b4b6651e0b4b87a3ac5196a8562e
GreG <prager.gergo@gmail.com>
2023-08-25 15:47:07 +0200

    Timetable added

--- lib/timetable_screen.dart
+++ lib/timetable_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:calendar_timeline/calendar_timeline.dart';
 
 class TimetableScreen extends StatelessWidget {
   const TimetableScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CalendarTimeline(
+        locale: 'hu',
+        monthColor: Theme.of(context).colorScheme.secondary,
+        activeBackgroundDayColor: Theme.of(context).colorScheme.onSecondary,
+        leftMargin: 25,
+        onDateSelected: (p0) {
+          print(p0);
+        },
+        firstDate: DateTime(2023),
+        lastDate: DateTime(2024),
+        initialDate: DateTime.now(),
+      ),
+    );
   }
 }
