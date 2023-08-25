f45893538dbb9764c8aac810de4ffbea9164dbf8
GreG <prager.gergo@gmail.com>
2023-08-25 17:27:16 +0200

    Targyak oldal kezdete

--- lib/subjects_screen.dart
+++ lib/subjects_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:neptunplus_flutter/exams_screen.dart';
 
 class SubjectsScreen extends StatelessWidget {
   const SubjectsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FutureBuilder(
+        future: getPeriodTermsList(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Expanded(
+              child: Column(children: []),
+            );
+          }
+          if (snapshot.hasError) {
+            return const Center(child: Icon(Icons.error_outline));
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
   }
 }
